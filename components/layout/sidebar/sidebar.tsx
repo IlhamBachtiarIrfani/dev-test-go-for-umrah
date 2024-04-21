31a80e3e80196fc2ffb851bd310b386ce79236f2
Ilham Bachtiar Irfani <irfanswag94@gmail.com>
2024-04-21 19:40:02 +0700

    [FIX] fix sidebar icons

--- components/layout/sidebar/sidebar.tsx
+++ components/layout/sidebar/sidebar.tsx
@@ -1,9 +1,14 @@
-import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
-
 import LogoSvg from "@ilhamirfan/public/logo-dark.svg"
+
+import LayoutIcon from '@ilhamirfan/public/icon/layout.svg'
+import CalendarIcon from '@ilhamirfan/public/icon/calendar.svg'
+import ReservationIcon from '@ilhamirfan/public/icon/reservation.svg'
+import BedIcon from '@ilhamirfan/public/icon/bed.svg'
+import StarIcon from '@ilhamirfan/public/icon/star.svg'
+import SettingIcon from '@ilhamirfan/public/icon/setting.svg'
 
 interface SidebarProps {
     show: boolean;
@@ -18,12 +23,12 @@ export default function Sidebar(props: SidebarProps) {
                 </Link>
             </div>
             <div className='navigation-container'>
-                <NavigationItem label='Dashboard' active />
-                <NavigationItem label='Rates & Availability' />
-                <NavigationItem label='Reservation' />
-                <NavigationItem label='Room' />
-                <NavigationItem label='Guest review' />
-                <NavigationItem label='Setting' />
+                <NavigationItem label='Dashboard' icon={<LayoutIcon />} active />
+                <NavigationItem label='Rates & Availability' icon={<CalendarIcon />} />
+                <NavigationItem label='Reservation' icon={<ReservationIcon />} />
+                <NavigationItem label='Room' icon={<BedIcon />} />
+                <NavigationItem label='Guest review' icon={<StarIcon />} />
+                <NavigationItem label='Setting' icon={<SettingIcon />} />
             </div>
         </aside>
     )
@@ -32,12 +37,13 @@ export default function Sidebar(props: SidebarProps) {
 interface NavigationItemProps {
     active?: boolean;
     label: string;
+    icon: React.ReactNode
 }
 
 function NavigationItem(props: NavigationItemProps) {
     return (
         <Link href={"#"} className={`navigation-item ${props.active ? "active" : ""}`}>
-            <div className='icon' />
+            {props.icon}
             <span>{props.label}</span>
         </Link>
     )
